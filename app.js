e565a4dbe6b41e5e55c154b6ec620170e2f61e9f
David Fulton <fultonium@gmail.com>
2017-10-21 12:30:01 -0500

    completed query code

--- app.js
+++ app.js
@@ -2,23 +2,48 @@ var searchTerms = "";
 var numResults = 0;
 var startYear = "";
 var endYear = "";
+var apiKey = "05f7127720fe4860ae0de8bea8301d88";
+
+var pullResults = function(){
+	searchTerms = $("#terms").val();
+	numResults = $("#num-results").val();
+	startYear = $("#start-year").val();
+	endYear = $("#end-year").val();
+
+	var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	queryURL += "&q=" + searchTerms + "&page=1";
+	if (startYear !== ""){
+		startYear += "0101";
+		queryURL += "&begin_date=" + startYear;
+	}
+	if (endYear !== ""){
+		endYear += "1231";
+		queryURL += "&end_date=" + endYear;
+	}
+	queryURL += "&api-key=05f7127720fe4860ae0de8bea8301d88";
 
 
+	$.ajax({
+	        url: queryURL,
+	        method: "GET"
+	      }).done(function(response) {
+
+	      	console.log(response);
 
 
-$.ajax({
-        url: queryURL,
-        method: "GET"
-      }).done(function(response) {
-
-
-
+	}
 
 }
 
+
+
 $("#search").on("click", function(){
-	pullResults
+	pullResults();
 
 })
 
-$("#clear").on("click", function()
+$("#clear").on("click", function(){
+	clearResults();
+
+
+})
