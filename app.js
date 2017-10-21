471f4cb0f13ef757369b09be6f61d894ce8236eb
David Fulton <fultonium@gmail.com>
2017-10-21 12:18:07 -0500

    added js file

---
+++ app.js
@@ -0,0 +1,24 @@
+var searchTerms = "";
+var numResults = 0;
+var startYear = "";
+var endYear = "";
+
+
+
+
+$.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+
+
+
+
+}
+
+$("#search").on("click", function(){
+	pullResults
+
+})
+
+$("#clear").on("click", function()
