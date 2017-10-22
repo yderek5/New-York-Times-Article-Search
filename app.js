34625827cad16f69d0429f596095de61f3f28888
yderek5 <yderek5@gmail.com>
2017-10-22 14:25:30 -0500

    currently functional

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var startYear =   0;
 var endYear =     0;
 
 // URL Base
-var querryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey;
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey;
 
 // Variable to Track number of articles
 var articleCouner = 0;
@@ -22,14 +22,46 @@ function runQuery(numArticles, queryURL) {
   $.ajax({url: queryURL, method: "GET"})
   .done(function(NYTData) {
     console.log(NYTData);
+    console.log(numArticles);
+    console.log(queryURL);
   });
 }
 
 // MAIN PROCESSES
 // ======================================
 
-$("#searchButton").on("click", function() {
-  runQuery();
+$("#searchBtn").on("click", function() {
+  // Get search term
+  queryTerm = $("#search").val().trim();
+
+  // Add in the Search Term
+  var newURL = queryURLBase + "&q=" + queryTerm;
+
+  // Get the number of records
+  numResults = $("#numRecords").val();
+
+  // get the start year and end year
+  startYear = $("#startYear").val().trim();
+  endYear = $("#endYear").val().trim();
+
+if(parseInt(startYear)) {
+
+// Add the necessary fields
+  startYear = startYear + "0101";
+// Add the date information to the URL
+  newURL = newURL + "&begin_date=" + startYear;
+}
+
+if(parseInt(endYear)) {
+
+// Add the necessary fields
+  endYear = endYear + "0101";
+// Add the date information to the URL
+newURL = newURL + "&end_date=" + endYear;
+}
+
+  // Send the AJAX Call the newly assembled URL
+  runQuery(numResults, newURL);
   return false;
 });
 
@@ -37,5 +69,4 @@ $("#searchButton").on("click", function() {
 // 2. Use those variables to run on AJAX call to the New York Times.
 // 3. Break down the NYT Object into useable fields
 // 4. Dynamically generate html content
-
 // 5. Dealing with "edge cases" -- bugs or situations that are not intuitive.
